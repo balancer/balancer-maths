df1c968b96b19d9504521f1c4881d2f8ca14f596
johngrantuk <johngrantuk@googlemail.com>
2024-07-24 10:51:06 +0100

    chore: Change to use prettier and eslint.

---
+++ typescript/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+  },
+};
