4ca4dd4bd231bfda75182598645ad0e30f6a7180
johngrantuk <johngrantuk@googlemail.com>
2024-11-25 16:19:39 +0000

    test: Formatting.

--- typescript/test/stablePool.test.ts
+++ typescript/test/stablePool.test.ts
@@ -1,3 +1,4 @@
+// pnpm test -- stablePool.test.ts
 import { describe, expect, test } from 'vitest';
 import { SwapKind, SwapParams } from '../src/index';
 import { Stable } from '../src/stable';
@@ -88,25 +89,26 @@ describe('stable pool', () => {
             });
         });
     });
-    describe('onSwap matches onchain results', () => {
-        // create new pool
-        // sim https://dashboard.tenderly.co/mcquardt/project/simulator/f174cf82-3525-4376-b13d-9e61bad1649c?trace=0
-        const tempPool = new Stable({
-            amp: 1000000n
+    describe('onSwap', () => {
+        test('matches onchain results', () => {
+            // sim https://dashboard.tenderly.co/mcquardt/project/simulator/f174cf82-3525-4376-b13d-9e61bad1649c?trace=0
+            const tempPool = new Stable({
+                amp: 1000000n,
+            });
+
+            const swapParams: SwapParams = {
+                swapKind: SwapKind.GivenIn,
+                amountGivenScaled18: 99500000000000000000n,
+                balancesLiveScaled18: [
+                    20000000000000000000000n,
+                    20000000000000000000000n,
+                ],
+                indexIn: 0,
+                indexOut: 1,
+            };
+
+            const amountOut = tempPool.onSwap(swapParams);
+            expect(amountOut).toEqual(99499505472260433154n);
         });
-
-        const swapParams: SwapParams = {
-            swapKind: SwapKind.GivenIn,
-            amountGivenScaled18: 99500000000000000000n,
-            balancesLiveScaled18: [
-                20000000000000000000000n,
-                20000000000000000000000n,
-            ],
-            indexIn: 0,
-            indexOut: 1,
-        };
-
-        const amountOut = tempPool.onSwap(swapParams);
-        expect(amountOut).toEqual(99499505472260433154n);
-    })
+    });
 });
